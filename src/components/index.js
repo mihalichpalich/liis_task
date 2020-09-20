9d0cb7d19828733b135c9e90b713c4278e0376cb
mihalichpalich <pavlovtfspb@gmail.com>
2020-09-21 00:18:21 +0300

    applied redux and redux-saga and created a server for the flights API

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as TabIndicator} from './TabIndicator';
-export {default as ScreenTitle} from './ScreenTitle';
-export {default as BrowseListItem} from './BrowseListItem/BrowseListItem';
-export {default as FavouritesListItem} from './FavouritesListItem/FavouritesListItem';
--- src/tabs/index.js
+++
@@ -1,2 +0,0 @@
-export {default as FlightsListFavourites} from './FlightsListFavourites';
-export {default as FlightsListBrowse} from './FlightsListBrowse';
